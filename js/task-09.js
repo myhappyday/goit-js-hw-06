e973645209ed02640f20f214fec19b5006e0cb2a
Marina Khmarskaya <myhappyday@ukr.net>
2023-02-28 07:09:48 +0200

    Add Task 10 Module-6

--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,13 @@
 const changeColorBtn = document.querySelector(".change-color");
 const colorSpan = document.querySelector(".color");
 
+changeColorBtn.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorSpan.textContent = getRandomHexColor();
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, "0")}`;
 }
-
-changeColorBtn.addEventListener("click", () => {
-  document.body.style.backgroundColor = getRandomHexColor();
-  colorSpan.textContent = getRandomHexColor();
-});
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
+const createBoxBtnEl = document.querySelector("[data-create]");
+const destroyBoxBtnEl = document.querySelector("[data-destroy]");
+const divBoxesEl = document.querySelector("#boxes");
+
+createBoxBtnEl.addEventListener("click", () => {
+  const amount = document.querySelector("input").value;
+  if (amount > 0 && amount <= 100) {
+    createBoxes(amount);
+  }
+});
+
+destroyBoxBtnEl.addEventListener("click", () => {
+  destroyBoxes();
+});
+
+function createBoxes(amount) {
+  const arrayBox = [];
+  let boxSize = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    const colorBox = getRandomHexColor();
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = colorBox;
+    arrayBox.push(box);
+    boxSize += 10;
+  }
+  divBoxesEl.append(...arrayBox);
+}
+
+function destroyBoxes() {
+  divBoxesEl.innerHTML = "";
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
